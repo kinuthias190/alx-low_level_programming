56ed44bd7c21dfc8c7dfb8e854d135a85d368c69
kinuthias190 <kinuthias190@github.com>
2022-09-15 10:46:33 -0700

    1. isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_isupper - checks if a  character is uppercase or not
+*@c: character to be tested
+*Return: 1 whether it is, 0 otherwise
+*/
+
+int _isdigit(int c)
+{
+	if ((c >= '48') && (c <= '57'))
+	{
+		return (1);
+	}
+
+	return (0);
+}
